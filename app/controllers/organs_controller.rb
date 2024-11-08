afe819aab819d5c1be06eb1bf80603573edad21a
Diego Anunciacao <diegoeliasanunciacao@gmail.com>
2024-11-08 00:33:38 +0000

    restrictions offers

--- app/controllers/organs_controller.rb
+++ app/controllers/organs_controller.rb
@@ -1,6 +1,6 @@
 class OrgansController < ApplicationController
   before_action :authenticate_user!
-
+  before_action :restrict_access, only: :show
   def index
     @organs = Organ.where.missing(:orders).where.not(user: current_user)
     if params[:query].present?
@@ -21,7 +21,7 @@ class OrgansController < ApplicationController
     @organ = Organ.new(organ_params)
     @organ.user = current_user
     if @organ.save
-      redirect_to organ_path(@organ), notice: 'Organ was successfully created.'
+      redirect_to my_offers_organs_path, notice: 'Organ was successfully created.'
     else
       render :new
     end
@@ -53,7 +53,18 @@ class OrgansController < ApplicationController
     @organs = Organ.where(user: current_user)
   end
 
+
+
+
   private
+
+  def restrict_access
+    organ = Organ.find(params[:id])
+    if organ.user_id != current_user.id || Order.where(organ_id: organ.id).present?
+      redirect_to root_path, alert: "You are not allowed to see this offer"
+    end
+  end
+
 
   def organ_params
     params.require(:organ).permit(:name, :description, :price, :age, :img_url, :robot_type)
